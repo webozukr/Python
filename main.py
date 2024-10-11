781f1a50f1a0e2b9601e4d60d2e72e840e515423
webozukr <zoliv@ukr.net>
2024-10-11 19:26:52 +0300

    Another commit

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-def greet(name):
-    return print(f"Hello World!!! And hello, {name.upper()}!")
-greet("mate")
+from datetime import datetime
+now = int(input("Введіть номер дня тижня: "))
+
+match now:
+    case 0:
+        print("Сьогодні у нас понеділок")
+    case 1:
+        print("Сьогодні у нас вівторок")
+    case 2:
+        print("Сьогодні у нас середа")
+    case 3:
+        print("Сьогодні у нас четвер")
+    case 4:
+        print("Сьогодні у нас п'ятниця")
+    case 5:
+        print("Сьогодні у нас субота")
+    case 6:
+        print("Сьогодні у нас п'неділя")
+    
